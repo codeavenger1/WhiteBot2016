00ca0e2fccfd9353a828c9daa802f1149e99f3bf
Nicholas Contreras <nicholasc2000@yahoo.com>
2016-04-14 19:55:45 -0700

    stuff with the robot --> square, and log distance

--- src/org/jointheleague/iaroc/Brain.java
+++ src/org/jointheleague/iaroc/Brain.java
@@ -1,5 +1,7 @@
 package org.jointheleague.iaroc;
 
+import android.os.SystemClock;
+
 import ioio.lib.api.IOIO;
 import ioio.lib.api.exception.ConnectionLostException;
 import org.wintrisstech.irobot.ioio.IRobotCreateAdapter;
@@ -9,6 +11,7 @@ import org.jointheleague.iaroc.sensors.UltraSonicSensors;
 public class Brain extends IRobotCreateAdapter {
     private final Dashboard dashboard;
     public UltraSonicSensors sonar;
+    int distance=0;
 
     public Brain(IOIO ioio, IRobotCreateInterface create, Dashboard dashboard)
             throws ConnectionLostException {
@@ -21,11 +24,32 @@ public class Brain extends IRobotCreateAdapter {
     public void initialize() throws ConnectionLostException {
         dashboard.log("Hello! I'm a Clever Robot!");
         //what would you like me to do, Clever Human?
+//        for (int i = 0; i<5; i++) {
+//            driveDirect(300, 300);
+//            SystemClock.sleep(2000);
+//            driveDirect(1, 90);
+//           SystemClock.sleep(4500);
+//
+//        }
 
-
-
+        readSensors(0);
 
     }
     /* This method is called repeatedly. */
-    public void loop() throws ConnectionLostException {}
+    public void loop() throws ConnectionLostException {
+
+        if (distance >= 1000)
+        {
+            driveDirect(0,0);
+        }
+
+        else{
+            driveDirect(50,50);
+        }
+        readSensors(0);
+        distance+=getDistance();
+        dashboard.log("" + distance);
+
+    }
+
 }
